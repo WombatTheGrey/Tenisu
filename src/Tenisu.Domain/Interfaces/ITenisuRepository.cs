34cc703a2ca9cb9023c87190219c8e8b28fcacb8
LocalWombat <LocalWombat@localhost>
2026-08-01 12:59:12 +0200

    TenisuRepository

---
+++ src/Tenisu.Domain/Interfaces/ITenisuRepository.cs
@@ -0,0 +1,16 @@
+﻿using Tenisu.Domain.Entities;
+
+namespace Tenisu.Domain.Interfaces
+{
+    public interface ITenisuRepository
+    {
+        ValueTask<Player?> GetPlayerByIdAsync(int playerId, CancellationToken cancellationToken);
+        Task<int> GetPlayersCountAsync(CancellationToken cancellationToken);
+        Task<IReadOnlyCollection<Player>> GetPagedPlayersAsync(int page, int pageSize, CancellationToken cancellationToken);
+        Task<IReadOnlyCollection<Player>> GetAllPlayersAsync(CancellationToken cancellationToken);
+        Task AddPlayerAsync(Player player, CancellationToken cancellationToken);
+
+        ValueTask<Country?> GetCountryAsync(string countryCode, CancellationToken cancellationToken);
+        Task AddCountryAsync(Country country, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Tenisu.Infrastructure/Repositories/TenisuRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tenisu.Domain.Entities;
+using Tenisu.Domain.Interfaces;
+using Tenisu.Infrastructure.Context;
+
+namespace Tenisu.Infrastructure.Repositories
+{
+    public class TenisuRepository : ITenisuRepository
+    {
+        private readonly TenisuDbContext _dbContext;
+
+        public TenisuRepository(TenisuDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public ValueTask<Player?> GetPlayerByIdAsync(int playerId, CancellationToken cancellationToken)
+        {
+            return _dbContext.Players.FindAsync([playerId], cancellationToken: cancellationToken);
+        }
+
+        public async Task<IReadOnlyCollection<Player>> GetAllPlayersAsync(CancellationToken cancellationToken)
+        {
+            var players = await _dbContext.Players
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+            return players.AsReadOnly();
+        }
+
+        public async Task<IReadOnlyCollection<Player>> GetPagedPlayersAsync(int page, int pageSize, CancellationToken cancellationToken)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(1, page);
+            ArgumentOutOfRangeException.ThrowIfLessThan(25, pageSize);
+
+            var playersQuery = _dbContext.Players.AsNoTracking()
+                .OrderBy(p => p.Id)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize);
+
+            return await playersQuery.ToListAsync(cancellationToken);
+        }
+
+        public Task<int> GetPlayersCountAsync(CancellationToken cancellationToken)
+        {
+            return _dbContext.Players.CountAsync(cancellationToken);
+        }
+
+        public async Task AddPlayerAsync(Player player, CancellationToken cancellationToken)
+        {
+            await _dbContext.AddAsync(player, cancellationToken);
+        }
+
+        public ValueTask<Country?> GetCountryAsync(string countryCode, CancellationToken cancellationToken)
+        {
+            return _dbContext.Countries.FindAsync([countryCode], cancellationToken: cancellationToken);
+        }
+
+        public async Task AddCountryAsync(Country country, CancellationToken cancellationToken)
+        {
+            await _dbContext.Countries.AddAsync(country, cancellationToken);
+        }
+    }
+}
