aa65e9161880ee7b58fcf4ca57c02eea85cd812a
LocalWombat <LocalWombat@localhost>
2026-08-05 00:11:47 +0200

    Tenisu.Infrastructure.Tests  (ongoing)

---
+++ Tests/Tenisu.Infrastructure.Tests/BaseRepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Tenisu.Infrastructure.Context;
+
+namespace Tenisu.Infrastructure.Tests
+{
+    public abstract class BaseRepositoryTests
+    {
+        private SqliteConnection _sqliteConnection;
+        protected TenisuDbContext DbContext;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            var cancellationToken = TestContext.CurrentContext.CancellationToken;
+            _sqliteConnection = new SqliteConnection("Filename=:memory");
+            await _sqliteConnection.OpenAsync(cancellationToken);
+
+            var options = new DbContextOptionsBuilder<TenisuDbContext>()
+                .UseSqlite()
+                .Options;
+
+            DbContext = new TenisuDbContext(options);
+
+            await DbContext.Database.MigrateAsync(cancellationToken);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _sqliteConnection.CloseAsync();
+            await _sqliteConnection.DisposeAsync();
+            await DbContext.DisposeAsync();
+        }
+
+    }
+}
---
+++ Tests/Tenisu.Infrastructure.Tests/TenisuRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Tenisu.Domain.Interfaces;
+using Tenisu.Infrastructure.Context;
+
+namespace Tenisu.Infrastructure.Tests
+{
+    [Category("Integration")]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class TenisuRepositoryTests : BaseRepositoryTests
+    {
+    }
+}
