a1b35e4868bd90743622ac5d98468ceaf4f7d112
LocalWombat <LocalWombat@localhost>
2026-08-06 08:40:16 +0200

    Tenisu.Application

---
+++ src/Tenisu.Application/Interfaces/IPlayerService.cs
@@ -0,0 +1,11 @@
+﻿using Tenisu.Domain.Entities;
+
+namespace Tenisu.Application.Interfaces
+{
+    public interface IPlayerService
+    {
+        Task<IReadOnlyList<Player>> GetAllPlayersAsync(CancellationToken cancellationToken);
+        Task<Player> GetPlayerAsync(int playerId, CancellationToken cancellationToken);
+        Task AddPlayer(Player player, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Tenisu.Application/Interfaces/IStatisticsService.cs
@@ -0,0 +1,9 @@
+﻿namespace Tenisu.Application.Interfaces
+{
+    public interface IStatisticsService
+    {
+        Task GetMostSuccesfullCountryAsync(CancellationToken cancellationToken);
+        Task GetAverageIMCAsync(CancellationToken cancellationToken);
+        Task GetMedianPlayerHeight(CancellationToken cancellationToken);
+    }
+}
